40e36cc39bf4a7caf544c721edf5b424ca04da13
Camille Villa <cvilla100@gmail.com>
2014-08-30 15:14:09 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+var days = [
+{
+    name: 'Ind Day',
+    day: 16
+},
+{
+    name: 'Labor Day',
+    day: 1
+},
+{
+    name: 'Laborious Day',
+    day: 2
+}
+];
+
+var t = $('table');
+t.addClass('classy');
+
+function insertEvent (name, day) {
+   if (name.length){
+       console.log('name is ' + name);
+   } else {
+       console.error('no name!');
+       return;
+   }
+    
+console.log(day);
+   
+   if (day > 0){
+       console.log('day is ' + name);
+   } else {
+       console.error('no day!');
+       return;
+   }
+   
+   var offset = 1;
+   
+   $($('td')[offset + (day -1)]).append('<span class="event">' + name + '</span>');
+}
+
+var btn = $('#submitter');
+btn.on('click', function(evt){
+   evt.preventDefault();
+   console.log('clicked button!');
+   var name = $('[name="eventName"]').val();
+   var day = $('[name="eventDay"]').val();
+   
+   insertEvent(name,day)
+});
+
+$.each(days, function (index,date){
+    insertEvent(date.name, date.day);
+})
+
+
+
+$('td').on('click', function (e) {
+    console.log(e);
+    
+    var day = $(e.target).children('.date').text();
+    console.log(day);
+    $('[name="eventDay"]').val(day);
+    
+});
+
+/*
+$('#eventName').on('keyup', function (event){
+    console.log($(this).val());
+});
+*/
+
+});
